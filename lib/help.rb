f32a9ba8348dcf840a653feefffdaed350795025
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-12-17 17:53:22 +0000

    Add options to help

--- lib/help.rb
+++ lib/help.rb
@@ -10,17 +10,26 @@ usag = " Usage: #{programname} <command> [options...]"
 def help
   print "\n  Usage: bossrun <command> [options...]\n"
   commands()
-  #options()
+  options()
 end
 
 def commands
   print "\n  Commands:\n"
-  print "     build         Builds the web-app\n"
-  print "     check         Checks if your config (.boss.yml) is ok and can be used\n"
-  print "     info          Fetchs information about the daemon and the web-app from the daemon\n"
-  print "     kill          Force the daemon to shutdown\n"
-  print "     log           Displays the log of the daemon running the web-app (usually #{ENV["BOSS_WEB_DIR"]}/.boss/logs/#{Time.now.strftime("%y%m%d")}.log)\n"
-  print "     pid           Displays the Process id for the daemon\n"
-  print "     start         Start the daemon to run the web-app\n"
-  print "     stop          Stops the daemon that is running the web-app\n"
+  print "     build             Builds the web-app\n"
+  print "     check             Checks if your config (.boss.yml) is ok and can be used\n"
+  print "     info              Fetchs information about the daemon and the web-app from the daemon\n"
+  print "     kill              Force the daemon to shutdown\n"
+  print "     log               Displays the log of the daemon running the web-app (usually #{ENV["BOSS_WEB_DIR"]}/.boss/logs/#{Time.now.strftime("%y%m%d")}.log)\n"
+  print "     pid               Displays the Process id for the daemon\n"
+  print "     start             Start the daemon to run the web-app\n"
+  print "     stop              Stops the daemon that is running the web-app\n"
+end
+
+def options
+  print "\n  Options:\n"
+  print "     -d, --no-daemon   Rather than run the web-app in the daemon, run it in the current terminal\n"
+  print "     -f, --force       Force the job\n"
+  print "     -h, --help        Output help\n"
+  print "     -v, --version     Builds the web-app\n"
+  print "     --verbose         Verbose mode\n"
 end
