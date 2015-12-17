2d25ca0c720ce3a0e20e4e9fbcb284e406097b6d
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-12-17 17:44:07 +0000

    Re-order commands in alphabetical order

--- lib/help.rb
+++ lib/help.rb
@@ -15,12 +15,12 @@ end
 
 def commands
   print "\n  Commands:\n"
-  print "     start         Start the daemon to run the web-app\n"
   print "     build         Builds the web-app\n"
-  print "     stop          Stops the daemon that is running the web-app\n"
   print "     check         Checks if your config (.boss.yml) is ok and can be used\n"
-  print "     log           Displays the log of the daemon running the web-app (usually #{ENV["BOSS_WEB_DIR"]}/.boss/logs/#{Time.now.strftime("%y%m%d")}.log)\n"
   print "     info          Fetchs information about the daemon and the web-app from the daemon\n"
-  print "     pid           Displays the Process id for the daemon\n"
   print "     kill          Force the daemon to shutdown\n"
+  print "     log           Displays the log of the daemon running the web-app (usually #{ENV["BOSS_WEB_DIR"]}/.boss/logs/#{Time.now.strftime("%y%m%d")}.log)\n"
+  print "     pid           Displays the Process id for the daemon\n"
+  print "     start         Start the daemon to run the web-app\n"
+  print "     stop          Stops the daemon that is running the web-app\n"
 end
